372a6f73c65032734e3542aaa1eade146dd93e80
Ricardo Pascal <voorloop@gmail.com>
2022-04-01 20:40:37 +0200

    write output to disk

--- src/output.py
+++ src/output.py
@@ -1,10 +1,8 @@
+from Bio import SeqIO
 
 
 def write_gbk(genome):
-    for contig in genome:
-        print(contig.format("gb"))
-        pass
-
-    #SeqIO.write(rec, handle, "genbank")
-    # handle.close()
+    with open("output.gbk", "w") as fh:
+        for contig in genome:
+            SeqIO.write(contig, fh, "genbank")
 
