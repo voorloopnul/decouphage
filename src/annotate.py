c1b286d9089cec2e65938dbbd4258fc42de55ee2
Ricardo Pascal <voorloop@gmail.com>
2022-05-04 20:11:34 +0200

    refactor annotation to single function

--- src/annotate.py
+++ src/annotate.py
@@ -39,31 +39,17 @@ class Annotate(object):
     def remove_species(stitle):
         return re.sub("[\(\[].*?[\)\]]", "", stitle)
 
-    def first_pass(self, _df):
-        bad_words = ["hypothetical", "putative", "unknown"]
+    def first_pass(self, _df, exclude_putative=True):
+        if exclude_putative:
+            bad_words = ["hypothetical", "putative", "unknown"]
+        else:
+            bad_words = []
+
         for index, row in _df.iterrows():
             if not any(word in row["stitle"] for word in bad_words):
                 blast_result = row.to_dict()
                 blast_result["stitle"] = self.remove_species(blast_result["stitle"])
                 logging.debug(f"1st: {blast_result}")
-                return blast_result
-        return None
-
-    def second_pass(self, _df):
-        for index, row in _df.iterrows():
-            blast_result = row.to_dict()
-            blast_result["stitle"] = self.remove_species(blast_result["stitle"])
-            logging.debug(f"2nd: {blast_result}")
-            return blast_result
-        return None
-
-    def third_pass(self, _df):
-        bad_words = ["hypothetical", "putative", "unknown"]
-        for index, row in _df.iterrows():
-            if not any(word in row["stitle"] for word in bad_words):
-                blast_result = row.to_dict()
-                blast_result["stitle"] = self.remove_species(blast_result["stitle"])
-                logging.debug(f"3rd: {blast_result}")
                 return blast_result
         return None
 
@@ -76,16 +62,18 @@ class Annotate(object):
             blast_result = self.first_pass(_df)
 
             if not blast_result:
-                blast_result = self.second_pass(_df)
+                blast_result = self.first_pass(_df, exclude_putative=False)
 
             if not blast_result:
-                blast_result = self.third_pass(self.get_df_for_spcific_gene(gene))
+                blast_result = self.first_pass(self.get_df_for_spcific_gene(gene))
+
+            if not blast_result:
+                blast_result = self.first_pass(self.get_df_for_spcific_gene(gene), exclude_putative=False)
 
             if blast_result:
                 qualifiers.append(blast_result)
             else:
                 logger.info(f"No good candidate found for CDS #{gene}")
-
 
         return qualifiers
 
