fad0718fee54ff9f3233b8bde1abc1cfa3c41b7b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 06:42:01 +0000

    Adding numeric_owner as keyword arguement

--- src/database.py
+++ src/database.py
@@ -69,7 +69,7 @@ def run_download():
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tarball, default_database_path)
